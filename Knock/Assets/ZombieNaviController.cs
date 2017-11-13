f566a94eefe753f84d487525a96d61dc1e056736
LAPTOP-LSSKFDV4\nky <nktyut66@gmail.com>
2017-11-13 19:33:00 +0900

    //怪物の行動パターンの試作です

---
+++ Knock/Assets/ZombieNaviController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieNaviController : MonoBehaviour {
+    private NavMeshAgent m_navMeshAgent;
+
+    //public Transform targetTF;
+
+    int r;
+    [SerializeField]
+    private bool isSeach = true;
+    private float m_timer;
+    [SerializeField]
+    private float SearchIntrval = 60f;
+
+	// Use this for initialization
+	void Start () {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_navMeshAgent.SetDestination(new Vector3(0, 0, 0));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isSeach)
+        {
+            m_timer -= Time.deltaTime;
+
+            if (m_timer <= 0)
+            {
+                r = Random.Range(-40, 40);
+
+                m_navMeshAgent.SetDestination(new Vector3(r, 0, r));
+                m_timer = SearchIntrval;
+            }
+        }
+	}
+}
