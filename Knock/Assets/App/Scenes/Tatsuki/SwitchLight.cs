f30f3f92cc0892339a359fa50cc2e5585adcc6fa
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2017-12-14 14:52:20 +0900

    [新規]懐中電灯のON/OFFの追加(右クリック)

---
+++ Knock/Assets/App/Scenes/Tatsuki/SwitchLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLight : MonoBehaviour {
+
+    GameObject spotlight; 
+
+
+    // Use this for initialization
+    void Start () {
+
+      spotlight =  GameObject.Find("FPSController").transform.Find("FirstPersonCharacter").transform.Find("Spotlight").gameObject;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            spotlight.SetActive(!spotlight.activeSelf);
+        }
+
+	}
+}
