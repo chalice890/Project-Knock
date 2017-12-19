5b1ccc31cb3556345765bec693c793efdca0e570
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2017-12-19 16:13:07 +0900

    [修正]Assetの追加、GameOverのバグを取り除いた

---
+++ Knock/Assets/SceneMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Main");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            SceneManager.LoadScene("Start");
+        }
+    }
+}
